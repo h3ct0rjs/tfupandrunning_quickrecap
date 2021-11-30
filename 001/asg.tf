df6ebe4da02b22084acb6ec88001a4b4cf8176df
Hector F. Jimenez <hfjimenez@utp.edu.co>
2021-11-29 22:03:19 -0500

    Fix bug with update of asg launch template :moon:

--- 001/asg.tf
+++ 001/asg.tf
@@ -1,10 +1,12 @@
 resource "aws_launch_configuration" "asg_lt" {
-  name                        = "cluster_ltconfig"
+  #name                        = "cluster_ltconfig" #there is a well know bug https://github.com/hashicorp/terraform/issues/3665
   image_id                    = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   user_data                   = <<-EOF
             #!/bin/bash
+            
             echo "Hello from DevOps Colombia" >index.html
+            ip addr show eth0 | grep inet | awk '{print $2}' >>index.html
             nohup busybox httpd -f -p 8080 &
             EOF
   security_groups             = [aws_security_group.http-sg-instance.id]
@@ -18,8 +20,8 @@ resource "aws_launch_configuration" "asg_lt" {
 resource "aws_autoscaling_group" "asg-web-cluster" {
   name                 = "asg-web-cluster"
   launch_configuration = aws_launch_configuration.asg_lt.name
-  min_size             = 2
-  max_size             = 4
+  min_size             = 8
+  max_size             = 10
   vpc_zone_identifier  = data.aws_subnet_ids.defaultsubnets.ids
   target_group_arns    = [aws_lb_target_group.tg-web-farm.arn]
   health_check_type    = "ELB"
