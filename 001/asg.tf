8a0a58b306d5c032057a4c93a10b2d3c5bac000e
Hector F. Jimenez <hfjimenez@utp.edu.co>
2021-12-02 15:46:01 -0500

    String Interpolation to inyect Values using template_file AutoScaling Group

--- 001/asg.tf
+++ 001/asg.tf
@@ -2,13 +2,7 @@ resource "aws_launch_configuration" "asg_lt" {
   #name                        = "cluster_ltconfig" #there is a well know bug https://github.com/hashicorp/terraform/issues/3665
   image_id                    = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  user_data                   = <<-EOF
-            #!/bin/bash
-            
-            echo "Hello from DevOps Colombia" >index.html
-            ip addr show eth0 | grep inet | awk '{print $2}' >>index.html
-            nohup busybox httpd -f -p 8080 &
-            EOF
+  user_data                   = data.template_file.user-data.rendered
   security_groups             = [aws_security_group.http-sg-instance.id]
   associate_public_ip_address = true
   iam_instance_profile        = data.aws_iam_role.ssmrole.name
@@ -34,5 +28,13 @@ resource "aws_autoscaling_group" "asg-web-cluster" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+
+data "template_file" "user-data" {
+  template = file("user-data.sh")
+  vars = {
+    serverport = var.server_port
   }
 }
--- 001/variables.tf
+++ 001/variables.tf
@@ -3,3 +3,9 @@ variable "name" {
   description = "Name of the EC2 Instance"
   default     = "first-ec2-server"
 }
+
+variable "server_port" {
+  type = number 
+  description = "Server port for ec2 instances"
+  default = 8080
+}
